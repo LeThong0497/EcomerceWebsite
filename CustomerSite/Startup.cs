51ca72c96d271205cd13ff5e76d61440a9b09076
thông lê <thonglethongle0497@gmail.com>
2021-03-30 11:22:37 +0700

    CustomerSite-Add Loggin with oidc

--- CustomerSite/Startup.cs
+++ CustomerSite/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,34 @@ namespace CustomerSite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+                .AddCookie("cookies")
+                .AddOpenIdConnect("oidc", options =>
+                 {
+                     options.Authority = "https://localhost:44341";
+                     options.RequireHttpsMetadata = false;
+                     options.GetClaimsFromUserInfoEndpoint = true;
+
+                     options.ClientId = "mvc";
+                     options.ClientSecret = "secret";
+                     options.ResponseType = "code";
+
+                     options.SaveTokens = true;
+
+                     options.Scope.Add("openid");
+                     options.Scope.Add("profile");
+                     options.Scope.Add("ecomerce.api");
+
+                     options.TokenValidationParameters = new TokenValidationParameters
+                     {
+                         NameClaimType = "name",
+                         RoleClaimType = "role"
+                     };
+                 });
             services.AddControllersWithViews();
         }
 
@@ -43,6 +72,8 @@ namespace CustomerSite
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
--- EcomerceWebsite-Backend/IdentityServer/IdentityServerConfig.cs
+++ EcomerceWebsite-Backend/IdentityServer/IdentityServerConfig.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using IdentityServer4;
 using IdentityServer4.Models;
-using System.Collections.Generic;
 
 namespace EcomerceWebsite_Backend.IdentityServer
 {
