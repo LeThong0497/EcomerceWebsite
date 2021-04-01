021fca3e5f2cc34c13de1301ef2587cd7774af33
thông lê <thonglethongle0497@gmail.com>
2021-04-01 10:25:50 +0700

    Add Identity

---
+++ EcomerceWebsite-Backend/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EcomerceWebsite_Backend.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EcomerceWebsite_Backend.Areas.Identity.IdentityHostingStartup))]
+namespace EcomerceWebsite_Backend.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
