8b2217ebc45e05f91948f5c0606727e0fa531dc2
thông lê <thonglethongle0497@gmail.com>
2021-04-02 16:49:18 +0700

    Update Model Class

--- EcomerceWebsite-Backend/Models/OrderDetail.cs
+++ EcomerceWebsite-Backend/Models/OrderDetail.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,14 @@ namespace EcomerceWebsite_Backend.Models
 {
     public class OrderDetail
     {
+        
         public int OrderID { get; set; }
+
         public int ProductID { get; set; }
         public int Quantity { get; set; }
-        
+
+        public virtual Product Product { get; set; }
+        public virtual Order Order { get; set; }
+
     }
 }
