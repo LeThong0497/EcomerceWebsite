ed40f26e319177719f84d1a2db3baf5285cd947b
thông lê <thonglethongle0497@gmail.com>
2021-04-05 11:12:42 +0700

    Update type of Name In Brand

--- EcomerceWebsite-Backend/Models/Brand.cs
+++ EcomerceWebsite-Backend/Models/Brand.cs
@@ -10,7 +10,7 @@ namespace EcomerceWebsite_Backend.Models
 
         [Required]
         [StringLength(60, MinimumLength = 3)]
-        public int Name { get; set; }                                                                                                                      
+        public string Name { get; set; }                                                                                                                      
 
         public virtual ICollection<Product> Products { get; set; }
 
