78eaa6b166c9bd71a39dd2cc567b24cf63d2ef1d
thông lê <thonglethongle0497@gmail.com>
2021-04-06 14:30:20 +0700

    Add Product view share

---
+++ ShareViewModel/ProductVm.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace ShareViewModel
+{
+    public class ProductVm
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string CPU { get; set; }
+
+        public string Screen { get; set; }
+
+        public string HardDrive { get; set; }
+
+        public string Card { get; set; }
+
+        public string Size { get; set; }
+
+        public string GateWay { get; set; }
+
+        public List<string> Image { get; set; }
+        public int BrandID { get; set; }
+        
+    }
+}
