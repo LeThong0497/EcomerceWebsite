881a33be755b38a1bef7fe40007af682d4a4b324
thông lê <thonglethongle0497@gmail.com>
2021-04-07 17:52:19 +0700

    Add View Component

---
+++ CustomerSite/ViewComponents/ProductMenuViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using CustomerSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerSite.ViewComponents
+{
+    public class ProductMenuViewComponent : ViewComponent
+    {
+        private readonly IProductClient _productClient;
+
+        public ProductMenuViewComponent(IProductClient brandApiClient)
+        {
+            _productClient = brandApiClient;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var Products = await _productClient.GetProduct();
+
+            return View(Products);
+        }
+
+        
+    }
+}
