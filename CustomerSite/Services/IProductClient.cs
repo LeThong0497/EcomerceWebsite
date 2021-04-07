1fb1db5ab70f4db8ecc6aea47cb90f5dd5494871
thông lê <thonglethongle0497@gmail.com>
2021-04-07 17:49:17 +0700

    Add service for Product

---
+++ CustomerSite/Services/IProductClient.cs
@@ -0,0 +1,12 @@
+﻿using ShareViewModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Services
+{
+    public interface IProductClient
+    {
+        Task<IList<ProductVm>> GetProduct();
+        Task<IList<ProductVm>> GetProductsByBrand(int id);
+    }
+}
---
+++ CustomerSite/Services/ProductClient.cs
@@ -0,0 +1,35 @@
+﻿using ShareViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Services
+{
+    public class ProductClient : IProductClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IList<ProductVm>> GetProduct()
+        {
+            var client = _httpClientFactory.CreateClient("local");
+            var response = await client.GetAsync("/api/Products");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsAsync<IList<ProductVm>>();
+        }
+
+        public async Task<IList<ProductVm>> GetProductsByBrand(int id)
+        {
+            var client = _httpClientFactory.CreateClient("local");
+            var response = await client.GetAsync("/api/Products/{id}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsAsync<IList<ProductVm>>();
+        }
+    }
+}
