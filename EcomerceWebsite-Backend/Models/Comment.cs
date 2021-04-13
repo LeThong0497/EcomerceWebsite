20fc3e53ddaa7f6b4add1cff5619009695f0da7e
thông lê <thonglethongle0497@gmail.com>
2021-04-13 15:43:34 +0700

    Update Comment Model Class

--- EcomerceWebsite-Backend/Models/Comment.cs
+++ EcomerceWebsite-Backend/Models/Comment.cs
@@ -7,11 +7,11 @@ namespace EcomerceWebsite_Backend.Models
     {
         [Key]
         public int CommentID { get; set; }
-        public int Rating { get; set; }
+        public int Star { get; set; }
         public DateTime Date { get; set; }
         public string Content { get; set; }
         public int ProductID { get; set; }
-
+        public string UserName { get; set; }
         public virtual Product Product { get; set; }
     }
 }
