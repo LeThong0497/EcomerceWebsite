1953f2a334d6c66be9f935617f6ff91128383e85
thông lê <thonglethongle0497@gmail.com>
2021-04-13 23:53:43 +0700

    Add product Controller

---
+++ CustomerSite/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using CustomerSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductClient _productApiClient;
+        private readonly IConfiguration _configuration;
+
+        public ProductController(IProductClient productApiClient, IConfiguration configuration)
+        {
+            _productApiClient = productApiClient;
+            _configuration = configuration;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productApiClient.GetProducts();
+
+            return View(products);
+        }
+
+        public async Task<IActionResult> GetProductByBrand(int id)
+        {
+            var products = await _productApiClient.GetProductsByBrand(id);
+
+            return View(products);
+        }
+       
+        
+        public async Task<IActionResult> Detail(int id)
+        {
+            var product = await _productApiClient.GetProduct(id);
+
+            return View(product);
+        }
+
+    }
+}
