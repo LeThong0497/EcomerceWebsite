4a259650059adaf5ae1488f372f1735b1adc6b3d
thông lê <thonglethongle0497@gmail.com>
2021-04-13 23:51:55 +0700

    Add Brand Controller

---
+++ CustomerSite/Controllers/BrandController.cs
@@ -0,0 +1,39 @@
+﻿using CustomerSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Controllers
+{
+    public class BrandController : Controller
+    {
+        private readonly IBrandClient _branclient;
+
+        private readonly IConfiguration _configuration;
+
+        public BrandController(IBrandClient brandClient, IConfiguration configuration)
+        {
+            _branclient = brandClient;
+            _configuration = configuration;
+        }
+       
+
+        public async Task<IActionResult> GetBrand(int id)
+        {
+            var product = await _branclient.GetBrand(id);
+
+            return View(product);
+        }
+
+
+        public async Task<IActionResult> GetBrands(int id)
+        {
+            var brands = await _branclient.GetBrands();
+
+            return View(brands);
+        }
+    }
+}
