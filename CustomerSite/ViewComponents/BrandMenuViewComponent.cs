2bbf76b5e4a03e46f3cdcb1e63110f2ecff711ce
thông lê <thonglethongle0497@gmail.com>
2021-04-13 23:52:47 +0700

    Add Brand ViewComponent

---
+++ CustomerSite/ViewComponents/BrandMenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using CustomerSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerSite.ViewComponents
+{
+    public class BrandMenuViewComponent: ViewComponent
+    {
+        private readonly IBrandClient _brandClient;
+
+        public BrandMenuViewComponent(IBrandClient brandClient)
+        {
+            _brandClient = brandClient;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var brands = await _brandClient.GetBrands();
+
+            return View(brands);
+        }
+    }
+}
