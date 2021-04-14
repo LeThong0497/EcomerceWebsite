052d947fffe11adf55d6a3fbbc240d2f61d263fe
thông lê <thonglethongle0497@gmail.com>
2021-04-14 22:57:48 +0700

    Add Commenting Share View

---
+++ ShareViewModel/CommentingVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShareViewModel
+{
+    public class CommentingVm
+    {
+        public int commentId { get; set; }
+
+        public int star { get; set; }
+
+        public DateTime date { get; set; }
+
+        public string content { get; set; }
+
+        public int productId { get; set; }
+
+        public string userName { get; set; }
+
+    }
+}
