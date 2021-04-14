e600240c1a176efd6f668fb8247b44386bbc545c
thông lê <thonglethongle0497@gmail.com>
2021-04-14 22:59:49 +0700

    Add Commenting Controller

---
+++ EcomerceWebsite-Backend/Controllers/CommentingController.cs
@@ -0,0 +1,41 @@
+﻿using EcomerceWebsite_Backend.Data;
+using EcomerceWebsite_Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShareViewModel;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EcomerceWebsite_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize("Bearer")]
+    public class CommentingController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public CommentingController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<StatusCodeResult> PostCommenting (FormCollection f,string userName,int productID)
+        {
+            var commentVm = new Comment();
+            commentVm.Star = Convert.ToInt32(f["radio"]);
+            commentVm.Date = DateTime.Today;
+            commentVm.Content = f["content"];
+            commentVm.ProductID = productID;
+            commentVm.UserName = userName;
+            _applicationDbContext.Comments.Add(commentVm);
+           await _applicationDbContext.SaveChangesAsync();
+
+             return StatusCode(200);
+        }
+    }
+}
