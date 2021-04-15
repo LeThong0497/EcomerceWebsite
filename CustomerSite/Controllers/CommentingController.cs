d9f1266b04f849605c3bf0784a05706f3201da72
thông lê <thonglethongle0497@gmail.com>
2021-04-15 17:02:29 +0700

    Add Commenting Controller

---
+++ CustomerSite/Controllers/CommentingController.cs
@@ -0,0 +1,38 @@
+﻿using CustomerSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ShareViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Controllers
+{
+    public class CommentingController : Controller
+    {
+        private readonly ICommentingClient _commentingClient;
+
+        private readonly IConfiguration _configuration;
+
+        public CommentingController(ICommentingClient commenting, IConfiguration configuration)
+        {
+            _commentingClient = commenting;
+            _configuration = configuration;
+        }
+        public async Task<ActionResult> PostCommenting(IFormCollection f,int productId,string userName)
+        {
+            var CommentingVm = new CommentingVm();
+            CommentingVm.star = Convert.ToInt32(f["rating"]);
+            CommentingVm.date = DateTime.Today;
+            CommentingVm.content = f["content"];
+            CommentingVm.productId = Convert.ToInt32(productId);
+            CommentingVm.userName = userName;
+            await _commentingClient.PostCommenting(CommentingVm);
+
+            string referer = Request.Headers["Referer"].ToString();
+            return Redirect(referer);
+        }
+    }
+}
