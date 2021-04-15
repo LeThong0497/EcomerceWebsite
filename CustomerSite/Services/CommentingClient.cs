da358419f2d97299d32c2f0442a7fced0e4b5f71
thông lê <thonglethongle0497@gmail.com>
2021-04-15 17:01:35 +0700

    Add Commenting Serviece

---
+++ CustomerSite/Services/CommentingClient.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using ShareViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Services
+{
+    public class CommentingClient : ICommentingClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public CommentingClient(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+        public async Task<HttpResponseMessage> PostCommenting(CommentingVm commentingVm)
+        {
+            var client = _httpClientFactory.CreateClient();
+            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(commentingVm),
+               Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(_configuration["BackendUrl:Default"]+ "/api/Commenting",httpContent);
+
+            return response.EnsureSuccessStatusCode();
+            
+        }
+
+       
+    }
+}
--- CustomerSite/Services/ICommentingClient.cs
+++ CustomerSite/Services/ICommentingClient.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShareViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace CustomerSite.Services
 {
    public interface ICommentingClient
     {
-        Task<StatusCodeResult> PostCommenting(FormCollection f,string userName,int productId);
+        Task<HttpResponseMessage> PostCommenting(CommentingVm commentingVm);
     }
 }
