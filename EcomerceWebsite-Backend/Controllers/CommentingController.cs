35553bdc5de2f31308587c305c855c4474721b13
thông lê <thonglethongle0497@gmail.com>
2021-04-15 16:52:41 +0700

    Update commenting Controller

--- EcomerceWebsite-Backend/Controllers/CommentingController.cs
+++ EcomerceWebsite-Backend/Controllers/CommentingController.cs
@@ -1,18 +1,15 @@
 ﻿using EcomerceWebsite_Backend.Data;
 using EcomerceWebsite_Backend.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ShareViewModel;
-using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace EcomerceWebsite_Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize("Bearer")]
+  
     public class CommentingController : Controller
     {
         private readonly ApplicationDbContext _applicationDbContext;
@@ -22,17 +19,17 @@ namespace EcomerceWebsite_Backend.Controllers
             _applicationDbContext = applicationDbContext;
         }
 
-        [HttpPost]
-        [Authorize]
-        public async Task<StatusCodeResult> PostCommenting (FormCollection f,string userName,int productID)
+         [HttpPost]
+         [AllowAnonymous]
+        public async Task<ActionResult> PostCommenting (CommentingVm commentingVm)
         {
-            var commentVm = new Comment();
-            commentVm.Star = Convert.ToInt32(f["radio"]);
-            commentVm.Date = DateTime.Today;
-            commentVm.Content = f["content"];
-            commentVm.ProductID = productID;
-            commentVm.UserName = userName;
-            _applicationDbContext.Comments.Add(commentVm);
+            var comment = new Comment();
+            comment.Star = commentingVm.star;
+            comment.Date = commentingVm.date;
+            comment.Content = commentingVm.content;
+            comment.ProductID = commentingVm.productId;
+            comment.UserName = commentingVm.userName;
+            _applicationDbContext.Comments.Add(comment);
            await _applicationDbContext.SaveChangesAsync();
 
              return StatusCode(200);
