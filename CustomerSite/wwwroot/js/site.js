03cd43e9467e0ba22062026c4441a3d683e49e87
thông lê <thonglethongle0497@gmail.com>
2021-04-15 17:07:07 +0700

    Css for  Product Detail

--- CustomerSite/wwwroot/js/site.js
+++ CustomerSite/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function SetImage(url) {
+    console.log(url)
+    $("#ProductImage").attr("src", url);
+}
+var quantitiy = 0;
+
+function Plus() {
+    //e.preventDefault();
+    var quantity = parseInt($('#quantity').val());
+    //document.getElementById("quantity").innerHTML = "" + quantity;
+    $('#quantity').val(quantity + 1);
+}
+
+function Minus() {
+    //e.preventDefault();
+    var quantity = parseInt($('#quantity').val());
+    if (quantity > 0) {
+        $('#quantity').val(quantity - 1);
+    }
+}
