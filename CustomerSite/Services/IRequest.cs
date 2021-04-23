5b36c2fc09a74c3e02a0ff0904ff6b02d5295126
thông lê <thonglethongle0497@gmail.com>
2021-04-23 12:26:18 +0700

    Add Request Class to get token

---
+++ CustomerSite/Services/IRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Services
+{
+    public  interface IRequest
+    {
+        Task<HttpClient> SendAccessToken();
+    }
+}
