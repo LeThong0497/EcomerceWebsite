5a91ff30a1e6c189d8c0c806e499fbc28f6424b9
thông lê <thonglethongle0497@gmail.com>
2021-04-27 23:24:22 +0700

    Add file decribe router

---
+++ Admin/adminsite/src/Configure/routerConfig.js
@@ -0,0 +1,10 @@
+export const LIST_PRODUCT = '/product';
+export const UPDATE_PRODUCT = '/updateProduct/:id';
+export const CREATE_PRODUCT = '/updateProduct/';
+
+export const HOME_PAGE = '/';
+export const LIST_USER = '/user';
+
+export const LIST_BRAND = '/brand';
+export const UPDATE_BRAND = '/updateBrand/:id';
+export const CREATE_BRAND = '/updateBrand/';
