a5be02e2fcc1ff7d5286f0aef15afe793c14e094
thông lê <thonglethongle0497@gmail.com>
2021-05-04 02:07:16 +0700

    Update class test

--- UnitTesst/TestController/BrandControllerTest.cs
+++ UnitTesst/TestController/BrandControllerTest.cs
@@ -1,4 +1,5 @@
 ﻿using EcomerceWebsite_Backend.Controllers;
+using EcomerceWebsite_Backend.Data;
 using EcomerceWebsite_Backend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,24 +11,24 @@ using Xunit;
 
 namespace UnitTesst.TestController
 {
-    public class BrandControllerTest
+    public class BrandControllerTest:IClassFixture<SqlLite>
     {
         private readonly SqlLite _fixture;
+        private readonly ApplicationDbContext _applicationDbContex;
 
         public BrandControllerTest(SqlLite fixture)
         {
             _fixture = fixture;
             _fixture.CreateDatabase();
-
+            _applicationDbContex = _fixture.Context;
         }
 
         [Fact]
         public async Task PostBrand_Success()
         {
-            var dbContext = _fixture.Context;
             var brand = new BrandCreateRequest { Name = "Test brand" };
 
-            var controller = new BrandsController(dbContext);
+            var controller = new BrandsController(_applicationDbContex);
             var result = await controller.PostBrand(brand);
 
             var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result.Result);
@@ -38,44 +39,42 @@ namespace UnitTesst.TestController
         [Fact]
         public async Task PutBrand_Success()
         {
-            var dbContext = _fixture.Context;
-            dbContext.Brands.Add(new Brand { Name = "Test brand" });
-            await dbContext.SaveChangesAsync();
+            _applicationDbContex.Brands.Add(new Brand { Name = "Test brand" });
+            await _applicationDbContex.SaveChangesAsync();
 
-            var oldCategory = await dbContext.Brands.OrderByDescending(x => x.BrandID).FirstAsync();
+            var oldCategory = await _applicationDbContex.Brands.OrderByDescending(x => x.BrandID).FirstAsync();
             var category = new BrandCreateRequest { Name = "Test put category" };
 
-            var controller = new BrandsController(dbContext);
+            var controller = new BrandsController(_applicationDbContex);
             var result = await controller.PutBrand(oldCategory.BrandID, category);
 
-            var returnValue = await dbContext.Brands.OrderByDescending(x => x.BrandID).FirstAsync();
+            var returnValue = await _applicationDbContex.Brands.OrderByDescending(x => x.BrandID).FirstAsync();
             Assert.Equal("Test put category", returnValue.Name);
         }
 
         [Fact]
         public async Task DeleteCategory_Success()
         {
-            var dbContext = _fixture.Context;
-            dbContext.Brands.Add(new Brand { Name = "Test category" });
-            await dbContext.SaveChangesAsync();
+            _applicationDbContex.Brands.Add(new Brand { Name = "Test category" });
+            await _applicationDbContex.SaveChangesAsync();
 
-            var oldCategory = await dbContext.Brands.OrderByDescending(x => x.BrandID).FirstAsync();
+            var oldCategory = await _applicationDbContex.Brands.OrderByDescending(x => x.BrandID).FirstAsync();
 
-            var controller = new BrandsController(dbContext);
+            var controller = new BrandsController(_applicationDbContex);
             var result = await controller.DeleteBrand(oldCategory.BrandID);
 
-            var returnValue = await dbContext.Brands.ToListAsync();
+            var returnValue = await _applicationDbContex.Brands.ToListAsync();
             Assert.Empty(returnValue);
         }
 
         [Fact]
         public async Task GetCategory_Success()
         {
-            var dbContext = _fixture.Context;
-            dbContext.Brands.Add(new Brand { Name = "Test category" });
-            await dbContext.SaveChangesAsync();
+           
+            _applicationDbContex.Brands.Add(new Brand { Name = "Test category" });
+            await _applicationDbContex.SaveChangesAsync();
 
-            var controller = new BrandsController(dbContext);
+            var controller = new BrandsController(_applicationDbContex);
             var result = await controller.GetBrands();
 
             var actionResult = Assert.IsType<ActionResult<IEnumerable<BrandVm>>>(result);
