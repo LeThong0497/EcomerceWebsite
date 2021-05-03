40320029d62dd53b76a76aae03f25b8ad1f3d76b
thông lê <thonglethongle0497@gmail.com>
2021-05-04 00:20:11 +0700

    Add UserVm

---
+++ ShareViewModel/UserVm.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ShareViewModel
+{
+    public class UserVm
+    {
+        public string UserID { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
